50377aa38b865c909fccdf3dc66f8644c050ec64
Kauan Sena <lkrs@protonmail.com>
2023-07-21 18:35:26 -0300

    Update server.py

--- Token application/production/database/server.py
+++ Token application/production/database/server.py
@@ -1,7 +1,8 @@
+# PostgreSQL Database
 database_info = {
-    "host": "containers-us-west-177.railway.app",
-    "database": "railway",
-    "user": "postgres",
-    "password": "3frhjPaS5p2o0UNWH5TH",
-    "port": "6531",
+    "host": "",
+    "database": "",
+    "user": "",
+    "password": "",
+    "port": "",
 }
